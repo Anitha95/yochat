6347baabed53235749ea70dcea9ef8bba6d35e83
Anitha_Nambi <anitha22695@gmail.com>
2017-12-06 16:36:14 +0530

    frontend js,css,html page

--- yochatbackend/src/test/java/com/niit/yochatbackend/test/UserTestCase.java
+++ yochatbackend/src/test/java/com/niit/yochatbackend/test/UserTestCase.java
@@ -23,15 +23,51 @@ public class UserTestCase {
 	public static void init()
 	{
 		context =  new AnnotationConfigApplicationContext();
-		context.scan("com.sbkchat.collaboration");
+		context.scan("com.niit.yochatbackend");
 		context.refresh();
 		
 		userDAO = (UserDAO) context.getBean("userDAO");
 		user = (User) context.getBean("user");
 	}
-	
+
+	@Test
+	public void addUserTestCase()
+	{
+		User user = new User();
+		
+		user.setFirstName("anitha");
+		user.setLastName("thirumalai");
+		user.setUsername("anitha");
+		user.setPassword("anitha95");
+		user.setEmailId("anitha22695@gmail.com");
+		user.setBirthDate(LocalDate.parse("1993-01-11")); //yyyy-mm-dd
+		user.setGender('F');
+		user.setPhone("9665224303");
+		user.setRole("User");
+		user.setStatus("APPROVED");
+		user.setEnabled(true);
+		user.setOnline(true);
+		user.setProfile("noDp.jpg");
+		
+		assertEquals("You have Successfully saved a user along with authorities inside the database!",true,userDAO.addUser(user));
+	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 
 
 
---
+++ yochatfrontend/WebContent/app/app.module.js
@@ -0,0 +1,15 @@
+var YOchat = angular.module('YOchat',[
+	'ngRoute',
+	'authenticationModule',
+	'userModule',
+	'blogModule',
+	'textAngular',
+	'blogCommentModule',
+	'forumModule',
+	'eventModule',
+	'jobModule',
+	'adminModule',
+	'requestModule',
+	'friendModule',
+	'chatModule'
+]);
